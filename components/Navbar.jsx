e69f3089c9dd592b997b91b74788ec841c399ff1
Avinash Kumar <97193810+avinash70008@users.noreply.github.com>
2022-03-21 21:03:17 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import { Link, Navigate } from "react-router-dom";
 
-const Navbar = () => {
+export const Navbar = () => {
   return (
-    <div className='navbar'>
-        <button>Home</button>
-        <button>History</button>
-        <button>Mystery</button>
-        <button>Mythology</button>
-        <button>Technology</button>
-    </div>
-  )
-}
-
-export default Navbar
+    <>
+      <div className="navbar">
+         <button onClick={() =>{
+          Navigate ("/section/Mystery");
+         }}>Mystery</button>
+         <button onClick={() =>{
+           Navigate("/section/Technology");
+         }}>Technology</button>
+         <button onClick={() =>{
+           Navigate("/section/Mythology");
+         }}>Mythology</button>
+         <button onClick={() =>{
+           Navigate("/section/history");
+         }}></button>
+         <button onClick={() =>{
+           Navigate("/");
+         }}></button>
+        {/* Populate 5 buttons with EXACT same classnames as of their routes name */}
+        {/* Example: 
+            <button className="history"> Link to history here  </button>  */}
+        {/* Home component will have `/` route and classname as `home`  */}
+      </div>
+    </>
+  );
+};
