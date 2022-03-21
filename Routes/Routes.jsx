dc4227b599682c5a2fffaa34ee033c929c7e8470
Avinash Kumar <97193810+avinash70008@users.noreply.github.com>
2022-03-21 21:02:12 +0530

    Update Routes.jsx

--- Routes/Routes.jsx
+++ Routes/Routes.jsx
@@ -4,19 +4,17 @@ import { Section } from "../Components/Section/Section";
 import { BookDetailsPage } from "../Components/BookDetailsPage/BookDetailsPage";
 import { NotFound } from "../Components/NotFound/NotFound";
 import { Navbar } from "../Components/Navbar/Navbar";
-import { Link } from "react-router-dom";
 
 export const AllRoutes = () => {
   return (
-     <div>
-        {/* <Route exact path="/" element={<Home />} /> */}
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        {/* <Route exact path="/section/Mystery" element={<Home />} /> */}
         {/* Create other routes here: Section, bookdetailspage and 404 */}
         {/*  */}
-        <Link to="/">Home</Link>
-        <Link to="/BookCard">Book Card</Link>
-        <Link to="/BookDetailsPage">Book Details Page</Link>
-        <Link to="/NotFound">Not Found</Link>
-        <Link to="/Section">Section</Link>
-        </div>
+      </Routes>
+    </>
   );
 };
