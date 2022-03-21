4361cecdd9957d536d816cb187c487d0b0a5a50f
Avinash Kumar <97193810+avinash70008@users.noreply.github.com>
2022-03-21 21:02:48 +0530

    Update Home.jsx

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,18 +1,46 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { BookCard } from "../BookCard/BookCard";
 
-const home = () => {
+import { SortAndFilterButtons } from "../SortAndFilterButtons/SortAndFilterButtons";
+
+export const Home = () => {
+  // get all books when user lands on the page
+  // populate them as mentioned below
+  const [dis, setDis] = useState([])
+  useEffect(() => {
+    axios.get("http://localhost:8080/books").then((response) => {
+      console.log(response);
+      setDis(response.data);
+    })
+  }, []);
+  const Main = styled.div`
+    /* Apply some responsive styling to children */
+  `;
+
   return (
-    <div className='homebar'>
-        <h1>Home</h1>
-        <div className='filters'>
-            <button>Sort by Title Asc</button>
-            <button>Sort by Title Desc</button>
-            <button>Sort by Price Asc</button>
-            <button>Sort by Price Desc</button>
+    <div className="homeContainer">
+      <h2 style={{ textAlign: "center" }}>Home</h2>
+      <SortAndFilterButtons
+        handleSort={
+          "give handleSort function to this component, that sorts books"
+        }
+      />
 
-        </div>
+      <Main className="mainContainer">
+        {/* 
+            Iterate over books that you get from network
+            populate a <BookCard /> component
+            pass down books id, imageUrl, title, price and anything else that you want to 
+            show in books Card.
+        */}
+        {dis.map((el) => {
+          return <BookCard key={el.id} id={el.id} title={el.title} price={el.price} />
+        })
+        }
+
+      </Main>
     </div>
-  )
-}
-
-export default home
+  );
+};
